ae43ad8e8dd29bbcc71517ae185b55fcf70a1a10
samuel.hames@uqconnect.edu.au <samuel.hames@uqconnect.edu.au>
2014-12-28 12:05:24 +0000

    Add __init__.py for BOF module

---
+++ __init__.py
@@ -0,0 +1,7 @@
+from featurelearning import Whiten, SphericalKMeans, HierSKMeans
+from imageencoding import BagOfFeaturesEncoder
+
+__all__ = ['Whiten',
+            'SphericalKMeans'
+            'HierSKMeans',
+            'BagOfFeaturesEncoder']
